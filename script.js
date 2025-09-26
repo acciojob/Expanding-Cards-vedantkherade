6ceb579e8e7243fb07d9c5e1599c1c42e35e3338
vedantkherade <152008775+vedantkherade@users.noreply.github.com>
2025-09-26 23:17:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let panels = document.querySelectorAll(".panel");
+    panels.forEach(panel=>{
+      panel.addEventListener("click",()=>{
+        panels.forEach(p=>p.classList.remove("active"));
+        panel.classList.add("active");
+      });
+    });
